1c14a853516b9f7c26ec60ee0badf789848bf268
stebou <sfinancerevolife@gmail.com>
2023-08-16 21:29:47 +0200

    little change

--- src/components/Videosection.jsx
+++ src/components/Videosection.jsx
@@ -5,12 +5,14 @@ function VideoSection() {
     <section className="flex justify-center items-center h-screen bg-gradient-to-b from-indigo-500 ...">
       <div className="shadow-2xl">
         <video 
-          width="640" 
-          height="360" 
-          controls
+          width="960"  // Increased width
+          height="540"  // Increased height
+          loop  // Video will loop
+          muted  // Video will be muted
+          autoPlay  // Video will autoplay (corrected attribute)
           className="rounded-lg"
         >
-          <source src="YOUR_VIDEO_URL_HERE.mp4" type="video/mp4" />
+          <source src="/sectionvid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
